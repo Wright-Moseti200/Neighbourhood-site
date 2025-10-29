70775110229123ab1a5d4ed1de6fea276c32ed08
Wright Gichana <wrightgichana@gmail.com>
2025-10-29 18:19:36 +0300

    commit

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,16 +4,18 @@ const { login, signUp, Upload, } = require("../controllers/userController");
 const { authMiddleware } = require("../middleware/authmiddleware");
 let userRouter = express.Router();
 
-//userRouter.post("/login",login);
-//userRouter.post("/signup",signUp);
-//userRouter.get("/getcredentials",authMiddleware);
-//userRouter.get("/getnews",);
-//userRouter.get("/getproducts",);
-//userRouter.get("/getjobs",);
-//userRouter.post("/post",authMiddleware)
-//userRouter.post("/uploaddocument",upload.single("document"),Upload);
-//userRouter.post("/uploadimage",upload.single("image"),Upload);
-//userRouter.post("/resume",authMiddleware,);
-//userRouter.get("/myapplication",authMiddleware,)
-//userRouter.get("/receivedapplication",authMiddleware)
+userRouter.post("/login",login);
+userRouter.post("/signup",signUp);
+userRouter.get("/getcredentials",authMiddleware);
+userRouter.get("/getnews",);
+userRouter.get("/getproducts",);
+userRouter.get("/getjobs",);
+userRouter.post("/post",authMiddleware)
+userRouter.post("/uploaddocument",upload.single("document"),Upload);
+userRouter.post("/uploadimage",upload.single("image"),Upload);
+userRouter.post("/resume",authMiddleware,);
+userRouter.get("/myapplication",authMiddleware,)
+userRouter.get("/receivedapplication",authMiddleware)
 userRouter.put("/updatestatus",authMiddleware,)
+
+module.exports={userRouter}
